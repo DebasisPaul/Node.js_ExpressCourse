3282960f5cd2a4e43d64f990af2e7997e03dd91f
debasispaul <idebasispaul@gmail.com>
2022-07-27 01:34:48 -0700

    // Code Staff added Through VS Code

---
+++ 2-globals.js
@@ -0,0 +1,15 @@
+
+// GLOBALS - NO WINDOW !!!
+
+// _dirname - path to current directory
+// _filename - file name
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// prodess - inso about env where the program is being executed
+
+
+
+console.log(_dirname)
+setInterval(() => {
+    console.log('Hello World')
+}, 1000)
